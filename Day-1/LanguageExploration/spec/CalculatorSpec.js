180941110db182337e2ec53a1808a7cec24a2431
tkmagesh <tkmagesh77@gmail.com>
2014-04-17 11:42:37 +0530

    Day -1

---
+++ Day-1/LanguageExploration/spec/CalculatorSpec.js
@@ -0,0 +1,111 @@
+describe("A calculator",function(){
+	it("Should be able to add two numbers",function(){
+		//arrange
+		var number1 = 10,
+			number2 = 20,
+			expectedResult = 30;
+
+		//act
+		var result = sum(number1,number2);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add two numbers in string format",function(){
+		//arrange
+		var number1 = "10",
+			number2 = "20",
+			expectedResult = 30;
+
+		//act
+		var result = sum(number1,number2);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add non numeric strings",function(){
+		//arrange
+		var number1 = "abc",
+			number2 = "xyz",
+			expectedResult = 0;
+
+		//act
+		var result = sum(number1,number2);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add functions returning numbers",function(){
+		//arrange
+		var f1 = function(){ return 10;},
+			f2 = function(){ return 20;},
+			expectedResult = 30;
+		
+
+		//act
+		var result = sum(f1,f2);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add functions returning numbers and non numeric strings",function(){
+		//arrange
+		var f1 = function(){ return 10;},
+			f2 = function(){ return "abc";},
+			expectedResult = 10;
+		
+
+		//act
+		var result = sum(f1,f2);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add functions returning functions returing numbers",function(){
+		//arrange
+		var f1 = function(){ return function(){ return 10;}},
+			f2 = function(){ return function(){ return 20;}},
+			expectedResult = 30;
+		
+
+		//act
+		var result = sum(f1,f2);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add only one number",function(){
+		//arrange
+		var number1 = 10,
+			expectedResult = 10;
+		
+
+		//act
+		var result = sum(number1);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+
+	it("Should be able to add variable length arguments",function(){
+		//arrange
+		var number1 = 10,
+			number2 = 20,
+			number3 = 30,
+			number4 = 40,
+			expectedResult = 100;
+		
+
+		//act
+		var result = sum(number1,number2,number3,number4);
+
+		//assert
+		expect(result).toBe(expectedResult);
+	});
+});
---
+++ Day-1/LanguageExploration/src/calculator.js
@@ -0,0 +1,11 @@
+function sum(x,y){
+	function parseArg(n){
+		if (typeof n === "function") return parseArg(n());
+		if (isNaN(n)) return 0;
+		return parseInt(n);
+	}
+	var result = 0;
+	for(var i=0;i<arguments.length;i++)
+		result += parseArg(arguments[i]);
+	return result;
+}
